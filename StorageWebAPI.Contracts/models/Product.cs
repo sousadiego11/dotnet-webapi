c2414390efa2a7be50b50c6cef4c8ba47fd7a84d
Diego Sousa <diegodev.geomais@gmail.com>
2024-04-10 22:02:12 -0300

    with gitignore

--- StorageWebAPI.Contracts/models/Product.cs
+++ StorageWebAPI.Contracts/models/Product.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StorageWebAPI.Contracts.models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Name { get; set; }
         public float Price { get; set; }
--- StorageWebAPI/obj/Debug/net8.0/StorageWebAPI.AssemblyInfo.cs
+++ StorageWebAPI/obj/Debug/net8.0/StorageWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StorageWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c4e7b75408570210706606606f73bb5d588c32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc2271ef6a4c642e6812738699416ea6133f76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StorageWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StorageWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
